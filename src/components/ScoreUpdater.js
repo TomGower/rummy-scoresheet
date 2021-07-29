95cbe7bddf263470d175f872d09fcdf1f0dc0902
TomGower <tom.gower@gmail.com>
2021-07-29 17:19:44 -0500

    Changed from explicit to implicit return

--- src/components/ScoreUpdater.js
+++ src/components/ScoreUpdater.js
@@ -1,22 +1,20 @@
 import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 
-const ScoreUpdater = ({ updateScore, players }, ref) => {
-  return (
-    <form onSubmit={updateScore} ref={ref}>
-      <div className="playerContainer">
-        {players.map((player) => (
-          <label htmlFor={player} key={player}>
-            {`Points for ${player}:`}
-            <input type="number" id={`${player}score`} name={player} />
-          </label>
-        ))}
-      </div>
-      <br />
-      <input type="submit" value="Update Scores" disabled={players.length === 0} />
-    </form>
-  );
-};
+const ScoreUpdater = ({ updateScore, players }, ref) => (
+  <form onSubmit={updateScore} ref={ref}>
+    <div className="playerContainer">
+      {players.map((player) => (
+        <label htmlFor={player} key={player}>
+          {`Points for ${player}:`}
+          <input type="number" id={`${player}score`} name={player} />
+        </label>
+      ))}
+    </div>
+    <br />
+    <input type="submit" value="Update Scores" disabled={players.length === 0} />
+  </form>
+);
 
 export default forwardRef(ScoreUpdater);
 
